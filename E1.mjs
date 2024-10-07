fa5ef1022d107947c16495db23118ef8a4915dfb
Damiansjj <ellis.viracocha.getial@student.ehb.be>
2024-10-07 10:25:52 +0200

    Update E1.mjs

--- E1.mjs
+++ E1.mjs
@@ -2,5 +2,22 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
-let vraag = parseFloat(await userInput.question("Geef de afmeting voor zijde 1: "));
-let vraag1 = parseFloat(await userInput.question("Geef de afmeting voor zijde 2: "));
+let zijde1;
+let zijde2;
+
+do
+{
+    zijde1 = parseFloat(await userInput.question("Geef het hoogte in: "));
+    zijde2 = parseFloat(await userInput.question("Geef het breedte in: "));
+
+}while(zijde1 > zijde2==zijde1<zijde2);
+
+
+for(let i = 0; i < zijde1; i++)
+{
+    for(let j = 1; j < zijde2; j++)
+    {
+        process.stdout.write(" * ")
+    }
+    console.log(" * ");
+}
