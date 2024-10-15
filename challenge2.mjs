00607735aba3126b88de3a22771d1d4d2ddced37
Damiansjj <ellis.viracocha.getial@student.ehb.be>
2024-10-15 10:31:52 +0200

    Create challenge2.mjs

---
+++ challenge2.mjs
@@ -0,0 +1,27 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+const userInput = readline.createInterface({ input, output });
+ 
+let hoogte = parseFloat(await userInput.question("Geef hoogte in: ")); 
+let outline = await userInput.question("Outline? true of false? ") === "true";  
+ 
+for (let i = 1; i <= hoogte; i++) {
+    let rij = '';
+ 
+ 
+    for (let j = 1; j <= (hoogte - i); j++) {
+        rij += ' ';
+    }
+ 
+    if (outline && i > 1 && i < hoogte) {
+        rij += '*';  
+        rij += ' '.repeat(2 * i - 3);  
+        rij += '*';  
+    } else {
+        rij += '*'.repeat(2 * i - 1); 
+    }
+ 
+    console.log(rij); 
+}
+ 
+userInput.close();
